cefa120d63819823e3f33240571290643f0ed4f6
Tati Micheletti <tati.micheletti@gmail.com>
2022-08-29 10:13:35 +0000

    Added raster saving to saveDisturbances -- only shapefiles previously

--- R/saveDisturbances.R
+++ R/saveDisturbances.R
@@ -1,14 +1,28 @@
 saveDisturbances <- function(disturbanceList, 
-                             currentTime){
+                             currentTime, overwrite){
   lapply(names(disturbanceList), function(Sector){
     whichLay <- names(disturbanceList[[Sector]])[!grepl(x = names(disturbanceList[[Sector]]), 
                                                         pattern = "potential")]
     lay <- lapply(whichLay, function(LAYER){
       Lay <- disturbanceList[[Sector]][[LAYER]]
-      Lay <- as(Lay, "Spatial")
-      writeOGR(obj = Lay, dsn = Paths[['outputPath']], 
-               layer = paste0("disturbances_", Sector, "_", LAYER, "_", currentTime), 
-               driver = "ESRI Shapefile", overwrite = overwrite) #also you were missing the driver argument
+      if (any(class(Lay) %in% c("SpatVector", "sf"),
+              is(Lay, "Spatial"))){
+        Lay <- as(Lay, "Spatial")
+        writeOGR(obj = Lay, dsn = Paths[['outputPath']], 
+                 layer = paste0("disturbances_", Sector, "_", LAYER, "_", currentTime), 
+                 driver = "ESRI Shapefile", overwrite = overwrite)
+      } else {
+        if (class(Lay) %in% c("RasterLayer", "SpatRaster")) {
+          Lay[] <- Lay[]
+          writeRaster(x = Lay, filename = file.path(Paths[['outputPath']],
+                                                    paste0("disturbances_", Sector, "_", 
+                                                           LAYER, "_", currentTime)), 
+                      format = "GTiff", overwrite = overwrite)
+        } else {
+          stop(paste0("Objects of class ", class(Lay), "can't be used in the module. ",
+                      "Please make sure all spatial objects are of raster, sp, sf or terra format."))
+        }
+      }
     })
   })
   message(crayon::green(paste0("Disturbances saved for ", currentTime)))
