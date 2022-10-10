2720485cfd549429f4fc40b8c21ef1f8549bd5ac
Tati Micheletti <tati.micheletti@gmail.com>
2022-10-10 09:18:56 +0000

    Added runName to save layers (replicates)

--- R/saveDisturbances.R
+++ R/saveDisturbances.R
@@ -1,5 +1,7 @@
 saveDisturbances <- function(disturbanceList, 
-                             currentTime, overwrite){
+                             currentTime, 
+                             overwrite,
+                             runName){
   lapply(names(disturbanceList), function(Sector){
     whichLay <- names(disturbanceList[[Sector]])[!grepl(x = names(disturbanceList[[Sector]]), 
                                                         pattern = "potential")]
@@ -9,14 +11,16 @@ saveDisturbances <- function(disturbanceList,
               is(Lay, "Spatial"))){
         Lay <- as(Lay, "Spatial")
         writeOGR(obj = Lay, dsn = Paths[['outputPath']], 
-                 layer = paste0("disturbances_", Sector, "_", LAYER, "_", currentTime), 
+                 layer = paste0("disturbances_", Sector, "_", LAYER, "_", currentTime, "_",
+                                runName), 
                  driver = "ESRI Shapefile", overwrite = overwrite)
       } else {
         if (class(Lay) %in% c("RasterLayer", "SpatRaster")) {
           Lay[] <- Lay[]
           writeRaster(x = Lay, filename = file.path(Paths[['outputPath']],
                                                     paste0("disturbances_", Sector, "_", 
-                                                           LAYER, "_", currentTime)), 
+                                                           LAYER, "_", currentTime, "_",
+                                                           runName)), 
                       format = "GTiff", overwrite = overwrite)
         } else {
           stop(paste0("Objects of class ", class(Lay), "can't be used in the module. ",
