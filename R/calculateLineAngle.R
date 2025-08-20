47b8eecb8682aa9fe7644af2dc3a4a01ae722098
RonCatK <ronald.kilian@protonmail.com>
2025-08-20 14:08:02 +0200

    now calls terra again

--- R/calculateLineAngle.R
+++ R/calculateLineAngle.R
@@ -6,6 +6,6 @@ calculateLineAngle <- function(Line) {
   }
   delta_y <- coords[2, 2] - coords[1, 2]
   delta_x <- coords[2, 1] - coords[1, 1]
-  angle <- atan2(delta_y, delta_x) * 180 / pi
+  angle <- terra::atan2(delta_y, delta_x) * 180 / pi # Convert to degrees
   return(angle)
 }
